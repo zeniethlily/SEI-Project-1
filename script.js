b122b2af02bf259d12d2752f6cb99f3125060816
Darrell Yeo <zeniethlily@gmail.com>
2020-06-19 13:51:39 +0800

    collision detection beta 1

--- script.js
+++ script.js
@@ -7,14 +7,31 @@ var y = canvas.height-30; //starting point of y. bottom of box
 var dx = 2;  //movement rate per frame
 var dy = -2;
 
-function draw(){  //draw code here
-    bContext.clearRect(0, 0, canvas.width, canvas.height); //clear frame before drawing
+var ballRadius = 10;
+
+
+function drawBall(){
     bContext.beginPath();
-    bContext.arc(x, y, 10, 0, Math.PI*2, false); //draw circle first 2 are position on canvas
+    bContext.arc(x, y, ballRadius, 0, Math.PI*2, false); //draw circle first 2 are position on canvas
     bContext.fillStyle = "#000000"; //fill color for the above shape
     bContext.fill();
     bContext.closePath();
-    x += dx;
+}
+
+function draw(){  //draw code here
+    bContext.clearRect(0, 0, canvas.width, canvas.height); //clear frame before drawing
+    drawBall(); //draws ball
+//--------------------ball collision detection---------------------------
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
+        dx = -dx; //if ball touches left or right "bounce" it by inverting the movement rate
+    }
+
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+        dy = -dy; //if ball touches top or bottom "bounce" it by inverting the movement rate
+    }
+
+
+    x += dx;    //moves the ball
     y += dy;
 }
 
