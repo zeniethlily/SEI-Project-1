0e78ac25a912a773772ce84b059adc8d49c4b294
Darrell Yeo <zeniethlily@gmail.com>
2020-06-26 15:46:30 +0800

    fixed scoring.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var bContext = canvas.getContext("2d");
 
 var randomColor = Math.floor(Math.random()*16777215).toString(16);
 var playerScore = 0;
+var playerLives = 3;
 
 var x = canvas.width/2;  //starting point of ball x. center
 var y = canvas.height-30; //starting point of ball y. bottom of box
@@ -80,6 +81,12 @@ function drawScore(){ //draws score on the canvas
     bContext.fillText(`Score: ${playerScore}`, 8, 20); //number behind x,y on canvas
 }
 
+function drawLives(){
+    bContext.font = "16px monospace";
+    bContext.fillStyle = "#000000";
+    bContext.fillText(`Lives: ${playerLives}`, canvas.width-95, 20); //drawn on the other side of the screen
+}
+
 function collisionDetection(){ //detection for blocks
     for(let yY = 0; yY < block.columns; yY++){
         for(let xX = 0; xX < block.rows; xX++){
@@ -89,7 +96,7 @@ function collisionDetection(){ //detection for blocks
                     dy = -dy; //bounces the ball after collision.
                     b.state = 0; //set state of the block to 0 so it doesn't get drawn in next frame.
                     playerScore += 12;
-                    if(score == block.rows * block.columns){
+                    if(playerScore == block.rows * block.columns){
                         //gameover
                         alert("Win!");
                         clearInterval(interval);
@@ -107,6 +114,7 @@ function draw(){  //draw code here
     drawBall(); //draws ball
     drawPlayer(); //draws player
     drawScore(); //draws score
+    drawLives(); //draws lives
 //--------------------ball collision detection---------------------------
     collisionDetection(); //for blocks
     if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
@@ -119,9 +127,18 @@ function draw(){  //draw code here
             dy = -dy;
         } else {
             //game end state because ball touches bottom bar. can add lives here.
-            alert("game over");
-            document.location.reload();
-            clearInterval(interval);
+            playerLives--;
+            if(!playerLives){
+                alert("Game Over!");
+                document.location.reload();
+                clearInterval(interval);
+            } else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                playerPos = (canvas.width - playerWidth) / 2;
+            }
         }
     }
 //--------------------player movement------------------------------------
